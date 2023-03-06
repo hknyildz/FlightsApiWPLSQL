a46579b7297bdf42ba499d8d54255ea1027fefc5
Hakan Yıldız <Aa5715805>
2023-03-06 06:52:43 +0300

    Swagger OpenAPI Added to project

--- src/main/java/com/hknyildz/FlightsApi/FlightsApiApplication.java
+++ src/main/java/com/hknyildz/FlightsApi/FlightsApiApplication.java
@@ -1,13 +1,30 @@
 package com.hknyildz.FlightsApi;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlightsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsApiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI openAPI(@Value("${application-description}") String description,
+						   @Value("${application-version}") String version){
+		return new OpenAPI()
+				.info(new Info()
+				.title("Sisal flight planning API")
+				.version(version)
+				.description(description)
+				.license(new License().name("Sisal flight planning API License")));
 	}
 
 }
